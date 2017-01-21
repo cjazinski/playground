77f850ec51d8ba4b7441d447a746a4e4288b6a35
Christopher Jazinski <chris@jazinski.com>
2017-01-21 11:46:41 -0600

    Added server-reload to gulp. localhost:8000

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var server = require('gulp-server-livereload');
 
 var sassPaths = [
   'bower_components/normalize.scss/sass',
@@ -7,8 +8,18 @@ var sassPaths = [
   'bower_components/motion-ui/src'
 ];
 
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(server({
+      port: 8000,
+      livereload: true,
+      defaultFile: 'index.html',
+      open: true
+    }));
+});
+
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
+  return gulp.src('app/scss/app.scss')
     .pipe($.sass({
       includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
@@ -17,9 +28,9 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task('default', ['sass','webserver'], function() {
+  gulp.watch(['app/scss/**/*.scss'], ['sass']);
 });
